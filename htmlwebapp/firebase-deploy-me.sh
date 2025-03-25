24100728a4600b5d9616c665bf19f7180231a834
root <root@hippoSurface.>
2025-03-25 13:32:02 +0800

    ai governance

---
+++ htmlwebapp/firebase-deploy-me.sh
@@ -0,0 +1,11 @@
+NO npm run build for this html project
+
+cd ./htmlwebapp
+
+Deploy only to aiStem
+$ firebase deploy --only hosting:aistem
+
+Deploy all sites together:
+$ firebase deploy --only hosting
+
+
